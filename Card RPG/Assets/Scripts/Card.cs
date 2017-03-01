9eb6badab9edbe15a1945925dcd71b95cd94473a
Amber <amberleejulian@gmail.com>
2017-03-02 10:04:12 +1300

    Made Card Class
    Made Card Class

---
+++ Card RPG/Assets/Scripts/Card.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Card : MonoBehaviour {
+
+
+    //PRIVATE VARIABLES:
+    enum ecardClass
+    {
+        DAMAGE, //normal attack and fire for now - fire long attack short
+        HEAL,
+        BLOCK,
+        NEXTTURNBOOST
+    }
+
+    enum ecardElement
+    {
+        PHYSICAL,
+        FIRE
+    }
+
+    //not sure how to do the image. Anne Senpai?
+    int tier;
+    string description;
+    ecardClass cardClass;
+    ecardElement cardElement;
+
+
+
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    ecardClass GetCardClass ()
+    {
+        return cardClass;
+    }
+
+}
