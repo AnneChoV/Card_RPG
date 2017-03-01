b18b1e6ab91d0bcc50d6a858bd82c8c5afb4e112
Anne <miss.anne.c@hotmail.com>
2017-03-02 09:58:38 +1300

    Basic Character and Enemy HP

---
+++ Card RPG/Assets/Scripts/EnemyStats.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyStats : MonoBehaviour {
+
+    public float enemyHealth;
+
+	// Use this for initialization
+	void Start () {
+        enemyHealth = 100f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Card RPG/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown("o"))
+        {
+            FindObjectOfType<PlayerStats>().playerHealth -= 10.0f;
+            Debug.Log("Hit Player!");
+            Debug.Log(FindObjectOfType<PlayerStats>().playerHealth);
+        }
+
+        if(Input.GetKeyDown("p"))
+        {
+            FindObjectOfType<EnemyStats>().enemyHealth -= 10.0f;
+            Debug.Log("Hit Enemy!");
+            Debug.Log(FindObjectOfType<EnemyStats>().enemyHealth);
+        }
+	}
+}
