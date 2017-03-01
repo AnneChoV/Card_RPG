49ddd8ebe9185afd8b59384c4fe733a87acbf896
Amber <amberleejulian@gmail.com>
2017-03-02 12:08:56 +1300

    Passing variables between Scenes

---
+++ Card RPG/Assets/Scripts/CombatManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CombatManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log(FindObjectOfType<Player>().health);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("o"))
+        {
+            FindObjectOfType<Player>().health -= 10.0f;
+            Debug.Log("Player was hit!");
+        }
+
+        if (Input.GetKeyDown("p"))
+        {
+            SceneManager.LoadScene("Test_Map");
+        }
+
+    }
+}
